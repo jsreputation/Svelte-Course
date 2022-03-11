82ff8bc840a06cc8a31ce70b9c93475274a289ef
Stone Tech <jsreputation@gmail.com>
2022-03-11 10:42:23 +0900

    feat: cart and product management by "store"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // import App from './App.svelte';
 // import App from './Assignment.svelte';
-import App from './MeetupTest.svelte';
+// import App from './MeetupTest.svelte';
 // import App from "./BindingAndForms.svelte";
 // import Header from './UI/Header.svelte';
 // import App from './EventTest.svelte';
-
+import App from './ProductTest.svelte';
 const app = new App({
 	// target: document.querySelector('#app'),
 	target: document.body,
