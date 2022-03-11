6632a70ba6811a346f8c7e4576a392383fda21c6
Intel Tech <jsreputation@gmail.com>
2022-03-11 23:40:35 +0900

    test: tweened from motion, cubicIn from easing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // import App from './App.svelte';
 // import App from './Assignment.svelte';
-import App from './MeetupTest.svelte';
+// import App from './MeetupTest.svelte';
 // import App from "./BindingAndForms.svelte";
 // import Header from './UI/Header.svelte';
 // import App from './EventTest.svelte';
 // import App from './ProductTest.svelte';
+import App from './AnimatingTest.svelte';
+
 const app = new App({
 	// target: document.querySelector('#app'),
 	target: document.body,
